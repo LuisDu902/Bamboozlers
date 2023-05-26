0a58541fe399b5b2b4b2d9a637d204b93091c531
Luis Du <up202105385@up.pt>
2023-05-26 13:46:10 +0100

    timer implemented

--- proj/src/model/game.c
+++ proj/src/model/game.c
@@ -14,16 +14,10 @@ void create_game_sprites()
     };
     xpm_map_t block_pixmaps[] = {(xpm_map_t)block_xpm};
     xpm_map_t level_pixmaps[] = {(xpm_map_t)map0_xpm};
-<<<<<<< HEAD
     xpm_map_t little_plank_pixmaps[] = {(xpm_map_t) little_plank_xpm, (xpm_map_t) little_plank_r1_xpm, (xpm_map_t) little_plank_r2_xpm, (xpm_map_t) little_plank_r3_xpm, (xpm_map_t) little_plank_r4_xpm, (xpm_map_t) little_plank_r5_xpm, (xpm_map_t) little_plank_r6_xpm, (xpm_map_t) little_plank_r7_xpm};
     xpm_map_t big_plank_pixmaps[] = {(xpm_map_t) big_plank_xpm, (xpm_map_t) big_plank_r1_xpm, (xpm_map_t) big_plank_r2_xpm, (xpm_map_t) big_plank_r3_xpm, (xpm_map_t) big_plank_r4_xpm, (xpm_map_t) big_plank_r5_xpm, (xpm_map_t) big_plank_r6_xpm, (xpm_map_t) big_plank_r7_xpm};
     xpm_map_t home_pixmaps[] = {(xpm_map_t) home_xpm};
     xpm_map_t timer_pixmaps[]= {(xpm_map_t) number_0_xpm, (xpm_map_t) number_1_xpm, (xpm_map_t) number_2_xpm, (xpm_map_t) number_3_xpm, (xpm_map_t) number_4_xpm, (xpm_map_t) number_5_xpm, (xpm_map_t) number_6_xpm, (xpm_map_t) number_7_xpm, (xpm_map_t) number_8_xpm ,(xpm_map_t) number_9_xpm};
-=======
-    xpm_map_t little_plank_pixmaps[] = {(xpm_map_t)little_plank_xpm, (xpm_map_t)little_plank_r1_xpm, (xpm_map_t)little_plank_r2_xpm, (xpm_map_t)little_plank_r3_xpm, (xpm_map_t)little_plank_r4_xpm, (xpm_map_t)little_plank_r5_xpm, (xpm_map_t)little_plank_r6_xpm, (xpm_map_t)little_plank_r7_xpm};
-    xpm_map_t big_plank_pixmaps[] = {(xpm_map_t)big_plank_xpm, (xpm_map_t)big_plank_r1_xpm, (xpm_map_t)big_plank_r2_xpm, (xpm_map_t)big_plank_r3_xpm, (xpm_map_t)big_plank_r4_xpm, (xpm_map_t)big_plank_r5_xpm, (xpm_map_t)big_plank_r6_xpm, (xpm_map_t)big_plank_r7_xpm};
-    xpm_map_t home_pixmaps[] = {(xpm_map_t)home_xpm};
->>>>>>> 4fc26bc (refactoring)
 
     home = create_sprite_xpm(home_pixmaps, 1, 500, 30);
   
@@ -33,14 +27,10 @@ void create_game_sprites()
     block = create_sprite_xpm(block_pixmaps, 1, 470, 500);
     little_plank = create_sprite_xpm(little_plank_pixmaps, 8, 500, 505);
     big_plank = create_sprite_xpm(big_plank_pixmaps, 8, 600, 505);
-<<<<<<< HEAD
     timer[0]=create_sprite_xpm(timer_pixmaps,10,100,505);
     timer[1]=create_sprite_xpm(timer_pixmaps,10,130,505);
     timer[2]=create_sprite_xpm(timer_pixmaps,10,160,505);
     timer[3]=create_sprite_xpm(timer_pixmaps,10,190,505);
-=======
-    map_items = (Sprite **)malloc(50 * sizeof(Sprite *));
->>>>>>> 4fc26bc (refactoring)
 }
 
 void destroy_game_sprites()
